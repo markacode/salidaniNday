ec18b1e827fcc10c9ab027c4fd6770606b6ba7e1
markacode <mac.markacode@gmail.com>
2020-03-10 09:01:26 +0800

    adding starting project

---
+++ build/precache-manifest.627d99b81256cca8f4362c5952b954c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367d67fbde9537420b711ad055ce9040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "926485a998bebcad3449",
+    "url": "/static/css/2.eaa14dea.chunk.css"
+  },
+  {
+    "revision": "8be2e3bb6cc8d2c2fa82",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926485a998bebcad3449",
+    "url": "/static/js/2.fd6227bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.fd6227bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be2e3bb6cc8d2c2fa82",
+    "url": "/static/js/main.f81db419.chunk.js"
+  },
+  {
+    "revision": "96b3c0d53e0a6f61592b",
+    "url": "/static/js/runtime-main.72c767c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
